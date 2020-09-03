91288b705cb8b4a80ab843b633e1bc4a4f6f2b49
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2020-09-03 15:32:15 +1000

    fixed minor issue

--- complexMapping/mapping.js
+++ complexMapping/mapping.js
@@ -1,14 +1,14 @@
 // Choose a complex mapping. Examples: z^2 => z.pow(2), 1/z => z.pow(-1), e^z => Complex.E.pow(z), sqrt(z) => z.sqrt()
 //[mouse]:draw, [t]:transform, [s]:show coords(none, rect, polar), [c]:clear lines,
-let fun = (z) => z.pow(2);//z.pow(-1);
+let fun = (z) => z.pow(2); //z.pow(-1);
 let scale = 2;
 
 let points = [];
 let transform = false;
 let coord = 1;
-let wfun = (z) => fun(z).mul(u).add(z.mul(1.0-u));
+let wfun = (z) => fun(z).mul(u).add(z.mul(1.0 - u));
 let u = 0.0;
-let rad= 3;
+let rad = 3;
 
 let sel;
 let system;
@@ -22,25 +22,30 @@ let drawing = false;
 
 
 var requested = {
-fn: function(z) { return {r:0,i:0}; },
-expr: '',
-canon: '',
-extent: 2,
-width: 0,
-height: 0,
-parameterized: false,
-animode: null,
-mode: null
+    fn: function (z) {
+        return {
+            r: 0,
+            i: 0
+        };
+    },
+    expr: '',
+    canon: '',
+    extent: 2,
+    width: 0,
+    height: 0,
+    parameterized: false,
+    animode: null,
+    mode: null
 };
 
 function setup() {
-	createCanvas(450, 450);
+    createCanvas(450, 450);
     colorMode(HSB, 1, 1, 1);
-	//unit = new Complex(1,1);
-	//unitr = new Complex(1,0);
-	//dim = new Complex(width, height);
-	//stroke(255);
-    
+    //unit = new Complex(1,1);
+    //unitr = new Complex(1,0);
+    //dim = new Complex(width, height);
+    //stroke(255);
+
     /*
     sel = createSelect();
     sel.position(10, 460);
@@ -59,19 +64,19 @@ function setup() {
     inp = createInput('z^2');
     inp.position(10, 460);
     inp.style('width:110px');
-    
+
     button = createButton('Transform');
     button.position(inp.x + 120, inp.y);
     button.mousePressed(transformButton);
-    
+
     checkbox = createCheckbox('Draw', false);
     checkbox.position(inp.x + 210, inp.y);
     checkbox.changed(myCheckedEvent);
-    
+
     buttonClear = createButton('Press C to clear');
-    buttonClear.position(inp.x + 210, inp.y+20);
+    buttonClear.position(inp.x + 210, inp.y + 20);
     //buttonClear.mousePressed(myButtonClear);
-    
+
     system = createSelect();
     system.position(inp.x + 335, inp.y);
     system.option('Cartesian');
@@ -79,19 +84,19 @@ function setup() {
     system.option('None');
     system.style('font-size', '18')
     system.changed(mySystemEvent);
-    
-   
+
+
     //inp.input(myInputEvent)
 }
 
 function myCheckedEvent() {
-    drawing = !drawing;//redraw();
-    if(drawing==false){
-     points = [];
+    drawing = !drawing; //redraw();
+    if (drawing == false) {
+        points = [];
     }
 }
 
-function myButtonClear(){
+function myButtonClear() {
     points = [];
 }
 
@@ -142,8 +147,8 @@ function myFunctionEvent(){
 }
  */
 
-function mySystemEvent(){
-    
+function mySystemEvent() {
+
     if (system.value() == 'Cartesian') {
         coord = 1;
     } else if (system.value() == 'Polar') {
@@ -151,93 +156,113 @@ function mySystemEvent(){
     } else if (system.value() == 'None') {
         coord = 3;
     }
-    
-    
+
+
 }
 
 function draw() {
-	background(0.1);
-    
+    background(0.1);
+
     cursor(HAND);
-    
-	if(transform) {
-		if(u < 1.0){
-		  u += 0.01;
-		}
-	} else {
-		if(u > 0.0){
-		  u -= 0.01;
-		}
-	}
-	if(coord==1){
-		for(let x=10; x<width; x += stepXY){
-			for(let y=10; y<height; y += stepXY){
-				noStroke();
+
+    if (transform) {
+        if (u < 1.0) {
+            u += 0.01;
+        }
+    } else {
+        if (u > 0.0) {
+            u -= 0.01;
+        }
+    }
+    if (coord == 1) {
+        for (let x = 10; x < width; x += stepXY) {
+            for (let y = 10; y < height; y += stepXY) {
+                noStroke();
                 let zx = map(x, 20, width, -scale, scale);
                 let zy = map(y, 20, height, -scale, scale)
                 let colorz = map(atan2(zy, -zx), -PI, PI, 0, 1);
-				fill(colorz, 1, 1);
-				let p = {x: x, y: y};
-				p = cmap({x: x, y: y});
+                fill(colorz, 1, 1);
+                let p = {
+                    x: x,
+                    y: y
+                };
+                p = cmap({
+                    x: x,
+                    y: y
+                });
                 noStroke();
-				ellipse(p.x, p.y, 2*rad);
-			}
-		}
-	} else if(coord==2){
-		for(let r=20; r<0.5*height; r += 20){
-			for(let a=-PI+0.01; a<=PI; a += 0.09){
-				noStroke();
-                let rnew = map(r, 0, 0.5*height, 0, 2);
+                ellipse(p.x, p.y, 2 * rad);
+            }
+        }
+    } else if (coord == 2) {
+        for (let r = 20; r < 0.5 * height; r += 20) {
+            for (let a = -PI + 0.01; a <= PI; a += 0.09) {
+                noStroke();
+                let rnew = map(r, 0, 0.5 * height, 0, 2);
                 let pcolorz = map(atan2(rnew * sin(a), -rnew * cos(a)), -PI, PI, 0, 1);
-				fill(pcolorz, 1, 1);
-				let p = {x: width/2+r*cos(a), y: height/2+r*sin(a)};
-				p = cmap({x: p.x, y: p.y});
+                fill(pcolorz, 1, 1);
+                let p = {
+                    x: width / 2 + r * cos(a),
+                    y: height / 2 + r * sin(a)
+                };
+                p = cmap({
+                    x: p.x,
+                    y: p.y
+                });
                 noStroke();
-				ellipse(p.x, p.y, 2*rad);
-			}
-		}
-	}
-    if(drawing==true){
-	if(mouseIsPressed && u<=0){
-		//points.push({x: mouseX, y: mouseY});
-        let cx = constrain(mouseX, 0, width);
-        let cy = constrain(mouseY, 0, height);
-        points.push(createVector(cx, cy));
-	}
+                ellipse(p.x, p.y, 2 * rad);
+            }
+        }
     }
-    
-	for(let i=1; i<points.length; i++){
-		let ppos = points[i-1];
-		let pos = points[i];
-		ppos = cmap(ppos);
-		pos = cmap(pos);
-		stroke(1,0,1);
-        strokeWeight(2);
-		line(ppos.x, ppos.y, pos.x, pos.y);
-	}
+    if (drawing == true) {
+        if (mouseIsPressed && u <= 0) {
+            //points.push({x: mouseX, y: mouseY});
+            let cx = constrain(mouseX, 0, width);
+            let cy = constrain(mouseY, 0, height);
+            points.push(createVector(cx, cy));
+        }
+    }
 
-    
+    for (let i = 1; i < points.length; i++) {
+        let ppos = points[i - 1];
+        let pos = points[i];
+        ppos = cmap(ppos);
+        pos = cmap(pos);
+        stroke(1, 0, 1);
+        strokeWeight(2);
+        line(ppos.x, ppos.y, pos.x, pos.y);
+    }
+
+
     //fill(1);
     //textSize(18);
     //stroke(0);
     //strokeWeight(1);
     //text('T: Apply', 20, 30);
-    
+
 }
 
-function transformButton(){
+function mousePressed() {
+    if (value === 0) {
+        value = 255;
+    } else {
+        value = 0;
+    }
+}
+
+function transformButton() {
     transform = !transform;
 }
 
-function keyTyped(){
-	switch (key){
-		case 't':
-			transform = !transform;
-		  break;
-		case 'c':
-			points = [];
-			break;
-	}
+function keyTyped() {
+    switch (key) {
+        case 't':
+            transform = !transform;
+            break;
+        case 'c':
+            points = [];
+            break;
+    }
 }
 
 function readinput() {
@@ -257,36 +282,36 @@ function readinput() {
         //if (!animate) {
         //  cancelplay();
         //}
-        
+
         //sethash();
         //redraw();
     }
 }
 
-function cmap(p){
-	let z = {
-		r: map(p.x, 0, width, -scale, scale),
-		i: map(p.y, 0, height, -scale, scale)
-	};
+function cmap(p) {
+    let z = {
+        r: map(p.x, 0, width, -scale, scale),
+        i: map(p.y, 0, height, -scale, scale)
+    };
     readinput();
-    
+
     //let zt = trimN(inp.value());
     //let parsed = complex_expression(zt);//Define function
-    
-    let w = requested.fn(z);//wfun(z);
-	let v = {
-		x: map(w.r * u + (1-u) * z.r, -scale, scale, 0, width),
-		y: map(w.i * u + (1-u) * z.i, -scale, scale, 0, height)
-	};
-	return {x: v.x, y: v.y};
+
+    let w = requested.fn(z); //wfun(z);
+    let v = {
+        x: map(w.r * u + (1 - u) * z.r, -scale, scale, 0, width),
+        y: map(w.i * u + (1 - u) * z.i, -scale, scale, 0, height)
+    };
+    return {
+        x: v.x,
+        y: v.y
+    };
 }
 
 function trimN(s) {
-if (s.trim) {
-    return s.trim();
+    if (s.trim) {
+        return s.trim();
+    }
+    return s.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
 }
-return s.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
-}
-
-
-
